19601ef5dd08073562ab312a334f0c3384debab3
WillWootton03 <willwootton03@gmail.com>
2026-01-18 13:15:55 -0800

    Added static root file for vercel

--- learning_logs/settings.py
+++ learning_logs/settings.py
@@ -148,10 +148,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static")
+     os.path.join(BASE_DIR, "theme/static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifetStaticFilesStorage"
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
