32195b87c314c1c60469da9a0430c515ec3a403d
WillWootton03 <willwootton03@gmail.com>
2026-01-18 14:13:37 -0800

    fixed tailwind static buildpath for styles

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 npm install
 
 echo "Building Tailwind CSS"
-npx tailwindcss -i ./theme/static/src/styles.css -o ./theme/static/dist/styles.css --minify
+npx tailwindcss -i ./theme/static/src/styles.css -o ./theme/static/css/dist/styles.css --minify
 
 echo "Collection static files"
 python manage.py collectstatic --noinput
