1b1797814cf0ce6865d15adf32da3d9555da6d76
WillWootton03 <willwootton03@gmail.com>
2026-01-18 14:47:40 -0800

    attempting again to load tailwind

--- build.sh
+++ build.sh
@@ -1,9 +1,1 @@
-cd theme
-npm install
-
-echo "Building Tailwind CSS"
-npx tailwindcss -i ./static_src/src/styles.css -o ./static/css/dist/styles.css --minify
-
-cd ..
-echo "Collection static files"
-python manage.py collectstatic --noinput
+pip install -r requirements.txt python3.12 manage.py collectstatic --noinput
